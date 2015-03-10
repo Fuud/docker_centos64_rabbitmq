9ea70d0b9ec0f14a8391dd12eec58f116b2ca27e
Fuud <fuudtorrentsru@gmail.com>
2015-03-10 10:35:06 +0300

    nohub

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-/etc/init.d/rabbitmq-server start &
-/usr/sbin/sshd -D &
-sleep `if [[ -z "$auto_exit_timeout" ]]; then echo infinity ; else echo $auto_exit_timeout ; fi`
+echo "will start tunnel at background"
+nohub socat TCP4-LISTEN:5673,fork TCP4:localhost:5672 &>/dev/null
+echo "background tunnel was started"
