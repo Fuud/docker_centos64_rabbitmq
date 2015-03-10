3598b3c5c8e445d516ee67202403bc7f69b5ef42
Fuud <fuudtorrentsru@gmail.com>
2015-03-10 11:42:13 +0300

    nohup

--- start_tunnel.sh
+++ start_tunnel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "will start tunnel at background"
-nohub socat TCP4-LISTEN:5673,fork TCP4:localhost:5672 &>/dev/null
+nohup socat TCP4-LISTEN:5673,fork TCP4:localhost:5672 &>/dev/null
 echo "background tunnel was started"
