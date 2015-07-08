eca36242b94f278bd2bf3abf0d5297eba82e4911
Fuud <fuudtorrentsru@gmail.com>
2015-07-08 13:40:29 +0300

    log directory

--- start_tunnel.sh
+++ start_tunnel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "will start tunnel at background"
+mkdir -p /var/log/socat
 nohup socat -v TCP4-LISTEN:5673,reuseaddr,fork TCP4:localhost:5672 &>/var/log/socat/socat.log &
-echo "background tunnel was started"
+echo "background tunnel was started. Logs can be found at /var/log/socat/socat.log"
